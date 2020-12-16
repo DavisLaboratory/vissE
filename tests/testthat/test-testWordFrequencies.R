e8a2f7a77bcc0ab15db68be211c60c09a3c0dbe6
bhuvad <bhuva.d@wehi.edu.au>
2020-12-16 18:58:38 +1100

    Added mode to use either term frequencies (TF) or inverse document frequency weighted term frequencies (TFIDF).

--- tests/testthat/test-testWordFrequencies.R
+++ tests/testthat/test-testWordFrequencies.R
@@ -1,4 +1,4 @@
-test_that("word frequencies are computed correctly", {
+test_that("word frequencies are computed correctly for edge cases", {
   #empty collections
   nullgsc = GSEABase::GeneSetCollection(list())
   freq = computeMsigWordFreq(nullgsc)
@@ -22,11 +22,30 @@ test_that("word frequencies are computed correctly", {
   expect_length(freq, 2)
   expect_equal(sapply(freq, ncol), c('Name' = 2, 'Short' = 2))
   expect_equal(sapply(freq, nrow), c('Name' = 0, 'Short' = 0))
+})
 
+test_that("word frequencies (TF) are computed correctly", {
   #non-empty collections
   data(hgsc)
   estgsc = hgsc[grep('ESTROGEN', hgsc)]
-  freq = computeMsigWordFreq(estgsc)
+  freq = computeMsigWordFreq(estgsc, measure = 'tf')
+
+  expect_length(freq, 2)
+  expect_equal(sapply(freq, nrow), c('Name' = 4, 'Short' = 5))
+  expect_equal(sapply(freq, class), c('Name' = 'data.frame', 'Short' = 'data.frame'))
+  expect_equal(freq$Name[freq$Name$word %in% c('early', 'late'), 2], c(1, 1))
+  expect_equal(freq$Short[freq$Short$word %in% c('early', 'late'), 2], c(1, 1))
+  expect_equal(freq$Name[freq$Name$word %in% c('estrogen'), 2], 2)
+  expect_equal(freq$Short[freq$Short$word %in% c('estrogen'), 2], 2)
+  expect_false('HALLMARK' %in% freq$Name$word)
+  expect_false('to' %in% freq$Short$word)
+})
+
+test_that("word frequencies (TFIDF) are computed correctly", {
+  #non-empty collections
+  data(hgsc)
+  estgsc = hgsc[grep('ESTROGEN', hgsc)]
+  freq = computeMsigWordFreq(estgsc, measure = 'tfidf')
 
   expect_length(freq, 2)
   expect_equal(sapply(freq, nrow), c('Name' = 4, 'Short' = 5))
