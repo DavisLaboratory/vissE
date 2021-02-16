895c9e02ab9c7d56a9df1198958c1ef6ead08b33
bhuvad <bhuva.d@wehi.edu.au>
2021-02-16 22:35:29 +1100

    Add script to generate a membership matrix given a gene set collection. Remove data object storing precomputed network.

---
+++ inst/scripts/generateMemMatrix.R
@@ -0,0 +1,40 @@
+library(msigdb)
+library(GSEABase)
+
+#----load collections----
+# msigdb.hs.SYM = msigdb.hs.SYM()
+# msigdb.mm.SYM = msigdb.mm.SYM()
+
+e = new.env()
+load('../msigdb/msigdb.hs.EZID.rda', envir = e)
+load('../msigdb/msigdb.mm.EZID.rda', envir = e)
+msigdb.hs.EZID = e$msigdb.hs.EZID
+msigdb.mm.EZID = e$msigdb.mm.EZID
+
+#----append KEGG----
+msigdb.hs.EZID = appendKEGG(msigdb.hs.EZID)
+msigdb.mm.EZID = appendKEGG(msigdb.mm.EZID)
+
+computeMemMatrix <- function(msigGsc) {
+  x = geneIds(msigGsc)
+  vals = unique(unlist(x))
+  
+  #create membership matrix for x
+  matx = Matrix::Matrix(
+    0,
+    nrow = length(x),
+    ncol = length(vals),
+    dimnames = list(names(x), vals),
+    sparse = TRUE
+  )
+  for (i in 1:length(x)) {
+    matx[i, ] = as.numeric(vals %in% x[[i]])
+  }
+  return(matx)
+}
+
+mem_mat_hs = computeMemMatrix(msigdb.hs.EZID)
+mem_mat_mm = computeMemMatrix(msigdb.mm.EZID)
+
+usethis::use_data(mem_mat_hs, internal = TRUE)
+usethis::use_data(mem_mat_mm, internal = TRUE)
