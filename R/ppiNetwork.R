b7ccc576aae4f2d0dd509344f2e8d5446c84a6bb
Dharmesh Bhuva <dharmesh.bhuva@adelaide.edu.au>
2023-03-20 16:04:18 +1030

    Fixed the edge case where networks of all groups are empty

--- R/ppiNetwork.R
+++ R/ppiNetwork.R
@@ -39,7 +39,7 @@ NULL
 #' grps = list('early' = 'HALLMARK_ESTROGEN_RESPONSE_EARLY', 'late' = 'HALLMARK_ESTROGEN_RESPONSE_LATE')
 #' ppi = msigdb::getIMEX(org = 'hs', inferred = TRUE)
 #' plotMsigPPI(ppi, hgsc, grps)
-#' 
+#'
 plotMsigPPI <-
   function(ppidf,
            msigGsc,
@@ -58,7 +58,7 @@ plotMsigPPI <-
     #check params
     checkNumericRange(nodeSF, 'nodeSF', 0)
     checkNumericRange(edgeSF, 'edgeSF', 0)
-    
+
     #compute combined graph
     gr = computeMsigGroupPPI(
       ppidf,
@@ -71,7 +71,7 @@ plotMsigPPI <-
       threshUseAbsolute,
       topN
     )
-    
+
     #plot base graph
     lytParams = c(list(graph = gr, layout = lytFunc), lytParams)
     p1 = do.call(ggraph::ggraph, lytParams) +
@@ -82,36 +82,46 @@ plotMsigPPI <-
         lwd = 0.5
       ) +
       ggraph::geom_node_point(aes(size = Degree, text = label), colour = '#FFFFFF') +
-      ggraph::geom_node_text(aes(label = Label), repel = TRUE) +
-      ggraph::geom_node_point(
-        aes(fill = geneStat, size = Degree),
-        alpha = 0.75,
-        shape = 21,
-        stroke = 0.2 * nodeSF
-      )
-    
-    #choose colour scale
-    if (is.null(geneStat)) {
-      p1 = p1 +
-        ggplot2::scale_fill_manual(values = c('A' = 1), na.value = 'orchid')
+      ggraph::geom_node_text(aes(label = Label), repel = TRUE)
+
+    if (all(is.na(as.data.frame(gr)$geneStat))) {
+      #if all empty, skip node drawing
+      warning('no PPIs found for any group')
     } else {
-      if (threshUseAbsolute & !all(geneStat >= 0)) {
-        lims = c(-1, 1) * stats::quantile(abs(geneStat), 0.99)
-        palname = 'vik'
-      } else {
-        lims = stats::quantile(abs(geneStat), c(0.01, 0.99))
-        palname = 'tokyo'
-      }
+      #if at least one graph is non-empty - draw
       p1 = p1 +
-        scico::scale_fill_scico(palette = palname, na.value = 'orchid', limits = lims, oob = scales::squish) +
-        ggplot2::labs(fill = 'statName')
+        ggraph::geom_node_point(
+          aes(fill = geneStat, size = Degree),
+          alpha = 0.75,
+          shape = 21,
+          stroke = 0.2 * nodeSF
+        )
+
+      #choose colour scale
+      if (is.null(geneStat)) {
+        p1 = p1 +
+          ggplot2::scale_fill_manual(values = c('A' = 1), na.value = 'orchid')
+      } else {
+        if (threshUseAbsolute & !all(geneStat >= 0)) {
+          lims = c(-1, 1) * stats::quantile(abs(geneStat), 0.99)
+          palname = 'vik'
+        } else {
+          lims = stats::quantile(abs(geneStat), c(0.01, 0.99))
+          palname = 'tokyo'
+        }
+        p1 = p1 +
+          scico::scale_fill_scico(palette = palname, na.value = 'orchid', limits = lims, oob = scales::squish) +
+          ggplot2::labs(fill = 'statName')
+      }
+
+      #node sizes
+      p1 = p1 + ggplot2::scale_size_continuous(range = c(0.1, 6) * nodeSF)
     }
-    
+
     #plot
     p1 = p1 +
       ggplot2::facet_wrap(~ Group, scales = 'free') +
       guides(size = guide_legend(ncol = 2)) +
-      ggplot2::scale_size_continuous(range = c(0.1, 6) * nodeSF) +
       bhuvad_theme() +
       ggplot2::theme(
         legend.position = 'bottom',
@@ -121,7 +131,7 @@ plotMsigPPI <-
         axis.text = element_blank(),
         axis.title = element_blank()
       )
-    
+
     return(p1)
   }
 
@@ -136,20 +146,20 @@ computeMsigGroupPPI <- function(ppidf,
                                 topN = 5,
                                 org = c('auto', 'hs', 'mm')) {
   #check params
-  checkGenesetCollection(msigGsc)
+  checkGenesetCollection(msigGsc, 'msigGsc')
   checkGroups(groups, names(msigGsc))
   if (!is.null(geneStat)) checkGeneStat(geneStat)
   checkNumericRange(threshConfidence, 'threshConfidence', 0, eq = TRUE)
   checkNumericRange(threshFrequency, 'threshFrequency', 0, eq = TRUE)
   checkNumericRange(topN, 'topN', 0, eq = TRUE)
-  
+
   #identify the organism
   org = match.arg(org)
   idType = msigdb::getMsigIdType(msigGsc)
   if (org == 'auto') {
     org = msigdb::getMsigOrganism(msigGsc, idType)
-  }  
-  
+  }
+
   #check PPI has records for organism of interest
   taxid = c('hs' = '9606', 'mm' = 10090)[org]
   isorg = ppidf$Taxid %in% taxid
@@ -157,31 +167,31 @@ computeMsigGroupPPI <- function(ppidf,
     ppidf = ppidf[isorg, , drop = FALSE]
   else
     stop('no PPIs found for organism, please ensure that the correct PPI database has been loaded.')
-  
+
   #retrieve PPI
   if (is(idType, 'SymbolIdentifier')) {
     colnames(ppidf)[colnames(ppidf) %in% c('SymbolA', 'SymbolB')] = c('from', 'to')
   } else {
     colnames(ppidf)[colnames(ppidf) %in% c('EntrezA', 'EntrezB')] = c('from', 'to')
   }
-  
+
   #compute group-specific PPIs
   ppi_list = lapply(names(groups), function(grpname) {
     gs = groups[[grpname]]
     genes = table(unlist(lapply(msigGsc[gs], GSEABase::geneIds)))
     ig = computeMsigPPI(names(genes), ppidf, threshConfidence)
-    
+
     #remove unconnected nodes
     ig = igraph::induced_subgraph(ig, V(ig)$name[igraph::degree(ig) > 0])
-    
+
     #if graph is not empty
     if (length(V(ig))>  0) {
       V(ig)$label = V(ig)$name
       V(ig)$Freq = genes[V(ig)$name] / length(gs)
-      
+
       #select frequent nodes
       isFreq = V(ig)$Freq >= threshFrequency
-      
+
       #add gene-stats if present
       if (!is.null(geneStat)) {
         V(ig)$geneStat = geneStat[V(ig)$name]
@@ -194,11 +204,11 @@ computeMsigGroupPPI <- function(ppidf,
         V(ig)$geneStat = NA
         keepStat = TRUE #keep all nodes
       }
-      
+
       #create unique node names for each group
       keep = isFreq & keepStat
       V(ig)$name = paste(V(ig)$name, grpname, sep = '_')
-      
+
       if (any(keep)) {
         ig = igraph::induced_subgraph(ig, V(ig)$name[keep])
         #remove unconnected nodes
@@ -209,12 +219,12 @@ computeMsigGroupPPI <- function(ppidf,
     }
     return(ig)
   })
-  
+
   #convert to data.frames
   names(ppi_list) = names(groups)
   nodedf = plyr::ldply(ppi_list, igraph::as_data_frame, what = 'vertices', .id = 'Group')
   edgedf = plyr::ldply(ppi_list, igraph::as_data_frame, what = 'edges', .id = 'Group')
-  
+
   #create NA entry for empty graphs
   nagrps = setdiff(names(groups), nodedf$Group)
   if (length(nagrps) > 0) {
@@ -230,7 +240,7 @@ computeMsigGroupPPI <- function(ppidf,
       )
     )
   }
-  
+
   #add labels
   #identify outliers
   nodedf = plyr::ddply(nodedf, 'Group', function(x) {
@@ -239,7 +249,7 @@ computeMsigGroupPPI <- function(ppidf,
       x$Label = ''
       return(x)
     }
-    
+
     #rank genes for labelling
     st = rank(x$Degree) * rank(x$Freq)
     if (!is.null(geneStat)) {
@@ -249,32 +259,32 @@ computeMsigGroupPPI <- function(ppidf,
     #add rank for webserver
     x$Rank = rank(-st)
     islab = !is.na(st) & rank(-st) <= topN
-    
+
     x$Label = ''
     x$Label[islab] = x$label[islab]
     return(x)
   })
-  
+
   #plot base graph
   gr = tidygraph::tbl_graph(nodedf, edgedf, node_key = 'name')
-  
+
   return(gr)
 }
 
 computeMsigPPI <- function(genes, ppidf, threshConfidence = 0) {
   colnames(ppidf)[colnames(ppidf) %in% c('Confidence')] = c('weight')
-  
+
   #identify the organism and use the subset PPI
   ppidf = ppidf[ppidf$from %in% genes &
                   ppidf$to %in% genes &
                   ppidf$weight >= threshConfidence, , drop = FALSE]
   ppidf = ppidf[ppidf$from != ppidf$to ,]
-  
+
   #create igraph
   edgecols = c('from', 'to', 'weight')
   ppidf = ppidf[, union(edgecols, colnames(ppidf)), drop = FALSE]
   ppi_ig = igraph::graph_from_data_frame(ppidf)
   igraph::V(ppi_ig)$Degree = igraph::degree(ppi_ig)
-  
+
   return(ppi_ig)
 }
